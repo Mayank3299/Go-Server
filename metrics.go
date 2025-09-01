ca46dbf7672bd566aee21c8d302e0e505936c30b
Mayank3299 <mayank.agarwal0903@gmail.com>
2025-09-01 13:51:24 +0530

    Add /admin namespace

--- metrics.go
+++ metrics.go
@@ -6,7 +6,13 @@ import (
 )
 
 func (ac *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	resp := fmt.Sprintf(`<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>`, ac.fileserverHits.Load())
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	resp := fmt.Sprintf("Hits: %d", ac.fileserverHits.Load())
 	w.Write([]byte(resp))
 }
