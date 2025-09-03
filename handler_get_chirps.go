fc9f948e59a202ad998bdd61f1925a4fae84eb92
Mayank3299 <mayank.agarwal0903@gmail.com>
2025-09-03 16:17:07 +0530

    Add author_id for get chirps

--- handler_get_chirps.go
+++ handler_get_chirps.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 func (ac *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	dbChirps, err := ac.db.GetChirps(r.Context())
@@ -9,15 +13,28 @@ func (ac *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	chirps := make([]Chirp, len(dbChirps))
-	for i, c := range dbChirps {
-		chirps[i] = Chirp{
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+			return
+		}
+	}
+
+	chirps := []Chirp{}
+	for _, c := range dbChirps {
+		if authorID != uuid.Nil && c.UserID.UUID != authorID {
+			continue
+		}
+		chirps = append(chirps, Chirp{
 			ID:        c.ID,
 			CreatedAt: c.CreatedAt,
 			UpdatedAt: c.UpdatedAt,
 			Body:      c.Body,
 			UserID:    c.UserID.UUID,
-		}
+		})
 	}
 
 	respondWithJSON(w, http.StatusOK, chirps)
